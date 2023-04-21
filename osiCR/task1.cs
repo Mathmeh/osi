d97ee4935337010d625a860dbce3ac829f78ea18
profandrew <fhvgggh.ru@gmail.com>
2023-04-21 12:57:25 +0300

    InitialCommit

---
+++ osiCR/task1.cs
@@ -0,0 +1,59 @@
+﻿using System.Diagnostics;
+
+namespace osiCR;
+
+public class task1
+{
+    private int _isActive;
+    private int PID1;
+    private int PID2;
+
+    public void Watcher()
+    {
+       PID1 = Convert.ToInt32(Console.ReadLine());
+       PID2 = Convert.ToInt32(Console.ReadLine());
+       
+       
+        Thread myThread1 = new Thread(obs);
+        myThread1.Start(PID1);
+        Thread.Sleep(30000);
+        
+        if (ProcessExists(PID1))
+        {
+            Console.WriteLine("timeout");
+        }
+    }
+
+    void obs(object? x)
+    {
+        int id = Convert.ToInt32(x);
+        while (true)
+        {
+            if (!ProcessExists(id))
+            {
+                IsActive = 0;
+                break;
+            }
+            else Thread.Sleep(10);
+        }
+    }
+    
+    public int IsActive
+    {
+        get { return _isActive; }
+        set
+        {
+            _isActive = value;
+            if (_isActive == 0)
+            {
+                var pr = Process.GetProcessById(PID2);
+                pr.Kill();
+            }
+        }
+    }
+    
+    private bool ProcessExists(int id)
+    {
+        return Process.GetProcesses().Any(x => x.Id == id);
+    }
+}
---
+++ osiCR/task2.cs
@@ -0,0 +1,70 @@
+﻿namespace osiCR;
+
+public class task2
+{
+    public int[,] matrixA;
+    public int[,] matrixB;
+    public int[,] matrixRES;
+    private int N;
+
+    public task2(int n)
+    {
+        N = n;
+        Random rand = new Random();
+        matrixA = new int[n, n];
+        matrixB = new int[n, n];
+        matrixRES = new int[n, n];
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                matrixA[i, j] = rand.Next(10);
+                matrixB[i, j] = rand.Next(10);
+            }
+        }
+        
+       PrintMatrix(matrixA);
+       Console.WriteLine();
+       PrintMatrix(matrixB);
+    }
+
+    public void MatrixSum()
+    {
+        List < Thread > threads= new List<Thread>();
+        for (int i = 0; i < N; i++)
+        {
+            var tr = new Thread(MatrixRowSum);
+            threads.Add(tr);
+        }
+
+        for (int i = 0; i < N; i++)
+        {
+            threads[i].Start(i);
+        }
+        Thread.Sleep(200);
+        Console.WriteLine();
+       PrintMatrix(matrixRES);
+    }
+
+    void MatrixRowSum(object? x)
+    {
+        for (int i = 0; i < N; i++)
+        {
+            matrixRES[(int)x, i] = matrixA[(int)x, i] - matrixB[(int)x, i];
+        }
+    }
+
+    void PrintMatrix(int[,] mtr)
+    {
+        for (int i = 0; i < N; i++)
+        {
+            string s = "";
+            for (int j = 0; j < N; j++)
+            {
+                s += mtr[i, j] + " ";
+            }
+            Console.WriteLine(s);
+        }
+    }
+}
